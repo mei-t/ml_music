ca52fe2f7396a04ec2a9cddfb823c92abea3155e
Mei Takahashi <me.7tspr@gmail.com>
2019-01-02 13:28:47 +0900

    sort keys to stavilize diff

--- before_commit.py
+++ before_commit.py
@@ -5,4 +5,9 @@ import codecs
 with codecs.open("music-playground.edit.ipynb", "r", "utf-8") as input:
     with codecs.open("music-playground.ipynb", "w", "utf-8") as output:
         loaded = json.load(input)
-        json.dump(loaded, output, separators=(',', ':'), ensure_ascii=False, indent=1)
+        json.dump(loaded, output,
+            separators=(',', ':'),
+            ensure_ascii=False,
+            indent=1,
+            sort_keys=True)
+
